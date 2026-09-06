8439014ca4816bf9239fd1b3bdcd5618b3d05ad6
Harmohat <harmohat_02@live.com>
2026-09-06 10:35:22 +0200

    feat: add Inventory model with productId and quantity

---
+++ inventory-service/src/main/java/com/harmohat/inventoryservice/model/Inventory.java
@@ -0,0 +1,19 @@
+package com.harmohat.inventoryservice.model;
+
+public class Inventory {
+	private int productId;
+	private int quantity;
+	
+	public Inventory(int productId, int quantity) {
+		this.productId = productId;
+		this.quantity = quantity;
+	}
+	
+	public int getProductId() {
+		return productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+}
