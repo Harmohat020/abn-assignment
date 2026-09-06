8103baebf9414dc4fb5934cc4d77ccde58c6bb55
Harmohat <harmohat_02@live.com>
2026-09-06 12:47:17 +0200

    feat: added Product model with BigDecimal price

---
+++ product-service/src/main/java/com/harmohat/productservice/model/Product.java
@@ -0,0 +1,28 @@
+package com.harmohat.productservice.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+	
+	private int id;
+	private String name;
+	private BigDecimal price;
+	
+	public Product(int id, String name, BigDecimal price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+}
